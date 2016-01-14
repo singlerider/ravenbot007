76ea72af840277f5652cbb4c12a2621712d77fa3
shane <haroboy876@gmail.com>
2016-01-13 20:22:03 -0800

    update top10 add enumeration to string return

--- src/lib/commands/top10.py
+++ src/lib/commands/top10.py
@@ -7,6 +7,6 @@ def top10():
     channel = globals.global_channel
     rank_data = db.get_top10(channel)
     # [(1, u'singlerider', 441, u'ravenbot007'), (2, u'nano_machina', 129, u'ravenbot007')]
-    candidates = ", ".join([x[1] + ": " + str(x[2]) for x in rank_data])
+    candidates = ", ".join([str(i + 1) + ") " + x[1] + ": " + str(x[2]) for i, x in enumerate(rank_data)])
     resp = "The top 10 cash holders are... " + candidates
     return resp
